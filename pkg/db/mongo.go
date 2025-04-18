4a38f15208158696f92a450eb261ec9ab417bd44
ntp <goonutza642@gmail.com>
2025-04-18 20:01:34 +0700

    add function connect db and close connection and encapsulate

--- pkg/db/mongo.go
+++ pkg/db/mongo.go
@@ -2,31 +2,58 @@ package db
 
 import (
 	"context"
-	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var client *mongo.Client
-
-func ConnectDB(uri string) *mongo.Client {
-	clientOptions := options.Client().ApplyURI(uri)
-
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		log.Fatal("Error connecting to MongoDB:", err)
-	}
-
-	err = client.Ping(context.Background(), nil)
-	if err != nil {
-		log.Fatal("Error pinging MongoDB:", err)
-	}
-
-	log.Println("Connected to MongoDB")
-	return client
+type MongoDBClient interface {
+	DB() (*mongo.Database, error)
+	Close() error
 }
 
-func GetCollection(dbName, collName string) *mongo.Collection {
-	return client.Database(dbName).Collection(collName)
+type mongoDBClient struct {
+	client *mongo.Client
+}
+
+func NewMongoDBConnection() (MongoDBClient, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	dbURL, err := getURLLocal()
+	if err != nil {
+		return nil, err
+	}
+
+	clientOptions := options.Client().ApplyURI(dbURL)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &mongoDBClient{client: client}, nil
+}
+
+func getURLLocal() (string, error) {
+	return "DB_URL", nil
+}
+
+func (c *mongoDBClient) DB() (*mongo.Database, error) {
+	return c.client.Database("DATABASE_NAME"), nil
+}
+
+func (c *mongoDBClient) Close() error {
+	ctx := context.TODO()
+	err := c.client.Disconnect(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
