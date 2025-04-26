c117dcdf137fbefe0ac72623c2361bb69ff7c4fc
ntp <goonutza642@gmail.com>
2025-04-26 14:28:42 +0700

    add middleware file for fiber middleware routes

---
+++ pkg/middleware/middleware.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"log"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/ntp7758/task-management/pkg/response"
+	"github.com/ntp7758/task-management/pkg/security"
+)
+
+func AuthMiddleware(c *fiber.Ctx) error {
+	tokenStr := c.Get("Authorization")
+	token := strings.TrimPrefix(tokenStr, "Bearer ")
+
+	if token == "" {
+		log.Println("authorization header is empty")
+		return response.FiberResponse(c, fiber.StatusUnauthorized, "authorization header is empty", nil)
+	}
+
+	claims, err := security.ParseJWTToken(tokenStr)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "unauthorized",
+		})
+	}
+
+	c.Locals("id", claims.Id)
+
+	return c.Next()
+}
