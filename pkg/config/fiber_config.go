2dcd260e93de60d281b7853e1a51ac074574e758
ntp <goonutza642@gmail.com>
2025-04-26 14:26:38 +0700

    add config file for fiber register middleware

---
+++ pkg/config/fiber_config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/ntp7758/task-management/pkg/middleware"
+)
+
+const (
+	logFilePath string = "logs/app.log"
+)
+
+func FiberConfig(app *fiber.App) {
+	app.Use(recover.New())
+
+	file, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	app.Use(logger.New(logger.Config{
+		Format: "${time} ${status} - ${latency} ${method} ${path}\n",
+		Output: file,
+	}))
+
+	app.Use(limiter.New(limiter.Config{
+		Max:        100,
+		Expiration: time.Minute * 1,
+	}))
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "*",
+		AllowHeaders: "",
+	}))
+
+	app.Use(compress.New(compress.Config{
+		Level: compress.LevelBestSpeed,
+	}))
+
+	app.Use(helmet.New())
+
+	app.Use(middleware.AuthMiddleware)
+}
